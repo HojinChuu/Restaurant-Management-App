481728c63afdd6fefe354b147753d0fa198ea639
HojinChuu <cnghwls7@gmail.com>
2020-07-29 00:36:36 +0900

    table crud

--- resources/views/management/inc/sidebar.blade.php
+++ resources/views/management/inc/sidebar.blade.php
@@ -6,7 +6,7 @@
         <a href="/management/menu" class="list-group-item list-group-item-action">
             <i class="fas fa-hamburger"></i> Menu
         </a>
-        <a href="" class="list-group-item list-group-item-action">
+        <a href="/management/table" class="list-group-item list-group-item-action">
             <i class="fas fa-chair"></i> Table
         </a>
         <a href="" class="list-group-item list-group-item-action">
