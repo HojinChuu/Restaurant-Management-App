a513fa2b096137d9bab67b82c59993658267d011
HojinChuu <cnghwls7@gmail.com>
2020-07-28 18:56:49 +0900

    category controller

---
+++ resources/views/management/category.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <div class="list-group">
+                    <a href="/management/category" class="list-group-item list-group-item-action">
+                        <i class="fas fa-align-justify"></i> Category
+                    </a>
+                    <a href="" class="list-group-item list-group-item-action">
+                        <i class="fas fa-hamburger"></i> Menu
+                    </a>
+                    <a href="" class="list-group-item list-group-item-action">
+                        <i class="fas fa-chair"></i> Table
+                    </a>
+                    <a href="" class="list-group-item list-group-item-action">
+                        <i class="fas fa-users-cog"></i> User
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <i class="fas fa-align-justify"></i> Category
+                <a href="{{ route('category.create') }}" class="btn btn-success btn-sm float-right">
+                    <i class="fas fa-plus"></i> Create Category
+                </a>
+                <hr>
+            </div>
+        </div>
+    </div>
+@endsection
